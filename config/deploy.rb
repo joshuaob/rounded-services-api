6c03f8229c19657e86140be6a00b0a9a02e5c641
Joshua Ob <joshuaaob@gmail.com>
2018-08-15 23:32:29 +0100

    wip

---
+++ config/deploy.rb
@@ -0,0 +1,58 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "rounded-services-api"
+set :repo_url, "git@gitlab.com:levastro/rounded-services-api.git"
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+      execute "mkdir #{shared_path}/log -p"
+    end
+  end
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      branch = fetch(:branch)
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc "Uploads env to all remote servers."
+  task :upload_env do
+    on roles(:app) do
+      invoke 'puma:make_dirs'
+      stage = fetch(:stage)
+      upload!(".env.#{stage}", "#{current_path}/.env")
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :upload_env
+  after  :finishing,    :cleanup
+end
