b2568d2ab567f7478d00083fcd619fd1773a6f93
Joshua Ob <joshuaaob@gmail.com>
2018-08-21 08:53:18 +0100

    add repository spec

--- spec/lib/repository_spec.rb
+++ spec/lib/repository_spec.rb
@@ -1,19 +1,61 @@
 module RoundedServices
   module Repository
     describe JobListing do
+      describe "#find_live"
+
       describe "#create" do
         it "creates a job listing"
       end
 
       describe "#update" do
-        it "updates a job listing"
+        let(:attributes_hash) do
+          attributes_hash = {
+            title: "senior full stack developer"
+          }
+        end
+
+        let(:existing_job_listing) { existing_job_listing = create(:job_listing) }
+
+        before do
+          form = RoundedServices::Form::JobListing.new(attributes_hash: attributes_hash)
+          described_class.new.update(form: form, reference: existing_job_listing.reference)
+          @job_listing = described_class.new.find_by_id(id: existing_job_listing.id)
+        end
+
+        it "updates dirty attributes" do
+          expect(@job_listing.title).to eq(attributes_hash[:title])
+        end
+
+        it "does not update clean attributes" do
+          expect(@job_listing.keywords).to eq(existing_job_listing.keywords)
+          expect(@job_listing.email).to eq(existing_job_listing.email)
+          expect(@job_listing.url).to eq(existing_job_listing.url)
+          expect(@job_listing.job_type).to eq(existing_job_listing.job_type)
+          expect(@job_listing.commute_type).to eq(existing_job_listing.commute_type)
+          expect(@job_listing.salary).to eq(existing_job_listing.salary)
+          expect(@job_listing.employer).to eq(existing_job_listing.employer)
+          expect(@job_listing.account_id).to eq(existing_job_listing.account_id)
+          expect(@job_listing.location).to eq(existing_job_listing.location)
+          expect(@job_listing.stripe_charge_id).to eq(existing_job_listing.stripe_charge_id)
+        end
+      end
+
+      describe "#find_by_id"
+
+      describe "#mark_as_inactive" do
+        it "sets a inactive at date on a job listing" do
+          job_listing = create(:job_listing)
+
+          JobListing.new.mark_as_inactive(reference: job_listing.reference)
+          inactive_job_listing = JobListing.new.find_by_reference(reference: job_listing.reference)
+
+          expect(inactive_job_listing.inactive_at).to be_truthy
+        end
       end
 
       describe "#mark_as_paid" do
         it "sets a paid at date on a job listing" do
-          account = Repository::Account.new.create(email: "joshuaaob@gmail.com")
-          form = Factory::JobListing.build(attributes_hash: {account: account})
-          job_listing = Factory::JobListing.create(form: form)
+          job_listing = create(:job_listing)
 
           JobListing.new.mark_as_paid(reference: job_listing.reference)
           paid_job_listing = JobListing.new.find_by_reference(reference: job_listing.reference)
@@ -24,9 +66,7 @@ module RoundedServices
 
       describe "#mark_as_published" do
         it "sets a published at date on a job listing" do
-          account = Repository::Account.new.create(email: "joshuaaob@gmail.com")
-          form = Factory::JobListing.build(attributes_hash: {account: account})
-          job_listing = Factory::JobListing.create(form: form)
+          job_listing = create(:job_listing)
 
           JobListing.new.mark_as_published(reference: job_listing.reference)
           published_job_listing = JobListing.new.find_by_reference(reference: job_listing.reference)
@@ -37,9 +77,7 @@ module RoundedServices
 
       describe "#update_stripe_charge_id" do
         it "sets a stripe charge id on a job listing" do
-          account = Repository::Account.new.create(email: "joshuaaob@gmail.com")
-          form = Factory::JobListing.build(attributes_hash: {account: account})
-          job_listing = Factory::JobListing.create(form: form)
+          job_listing = create(:job_listing)
 
           JobListing.new.update_stripe_charge_id(reference: job_listing.reference, stripe_charge_id: "123")
           paid_job_listing = JobListing.new.find_by_reference(reference: job_listing.reference)
